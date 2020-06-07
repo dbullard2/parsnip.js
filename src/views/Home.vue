b6c59e8801886714e92da564dc537c1ae8316f83
dbullard2 <daniel.earl.bullard@gmail.com>
2020-06-07 13:16:39 -0500

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,75 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<HelloWorld msg="Welcome to Your Vue.js App" />-->
+    <div class="headerimage">
+      <p>hello</p>
+    </div>
+    <div id="backshape">
+      <div class="wrapper"></div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
-}
+    //HelloWorld,
+  },
+};
 </script>
+
+<style scoped>
+* {
+  -moz-box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+}
+
+@font-face {
+  font-family: 'walsheim';
+  src: url('../assets/GT-Walsheim-Regular.otf');
+}
+
+.headerimage {
+  background-color: #083d08;
+  height: 600px;
+  width: 100%;
+  background-size: cover;
+  position: relative;
+  z-index: -1;
+  color: #fff;
+  font-size: 40px;
+  font-family: 'walsheim';
+}
+
+#backshape .wrapper {
+  display: block;
+  float: left;
+  margin: 50px auto 200px;
+  width: 100%;
+  padding: 0 5%;
+  transform: skew(0deg, -5deg);
+  -ms-transform: skew(0deg, -10deg);
+  -webkit-transform: skew(0deg, -5deg);
+}
+
+#backshape {
+  z-index: 1;
+  display: block;
+  float: left;
+  margin-top: -100px;
+  width: 100%;
+  background: white;
+  transform: skew(0deg, 10deg);
+  -ms-transform: skew(0deg, 10deg); /* IE 9 */
+  -webkit-transform: skew(0deg, 5deg); /* Safari and Chrome */
+}
+</style>
